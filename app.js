3c02bad7aeb51596a1bed8c0038e9a694f9a16a2
Nicolasmorahernandez <nicolasmorahernandez@hotmail.com>
2025-02-06 08:42:13 -0500

    Update app.js
    1. Se creó la función de limpiar la caja para cada vez que se presiona el botón añadir.
    2. Se creó la función actualizarLista, esta va mostrando una lista de los amigos que se va añadiendo.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let ListaNombresParticipantes =[];
 function agregarAmigo(){
     //Solicitar el nombre de los participantes del juego
     let NombresAmigos = document.getElementById("amigo");
-    let Nombre = NombresAmigos.value.trim();
+    let Nombre = NombresAmigos.value;
 
     if(Nombre === ""){
         alert("Por favor, inserte un nombre.");
@@ -15,6 +15,26 @@ function agregarAmigo(){
     ListaNombresParticipantes.push(Nombre);
     
     console.log("Lista de nombre", ListaNombresParticipantes);
+
+    limpiarCaja();
+    actualizarLista();
 }
 
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+function actualizarLista(){
+
+    let lista= document.getElementById("listaAmigos");
+    lista.innerHTML="";
+
+    // Recorrer el array LstaNombresParticipantes y agregar cada nombre como <li>
+    for (let i = 0; i < ListaNombresParticipantes.length; i++) {
+        let li = document.createElement("li"); // Crear un nuevo elemento <li>
+        li.textContent = ListaNombresParticipantes[i]; // Asignar el nombre del amigo
+        lista.appendChild(li); // Agregar el <li> a la lista
+    }
+
+}
 
